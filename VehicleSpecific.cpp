d758f38d987b69a2316447bd9de014b568fcff1a
Collin Kidder <collink@kkmfg.com>
2019-10-07 20:45:13 -0400

    Added some code to VehicleSpecific for a project of mine. It can serve as a real world example of how this module might look.

--- VehicleSpecific.cpp
+++ VehicleSpecific.cpp
@@ -28,12 +28,13 @@
  * devices in order to create a custom control system for your car. Each main device has configuration options
  * and is meant for generalized usage. But, what if you want to go further? Perhaps you've got a PowerKeyPro
  * keypad and you want to be able to select different regen powers or drive power settings? The default device
- * drivers don't support such things because they're not very generic. Or, perhaps you want to allow
+ * drivers don't support such things because they're generic. Or, perhaps you want to allow
  * your BMS and motor controller to talk? Maybe the BMS can limit regen or drive power automatically based on battery
  * conditions. The code doesn't currently support such things but it would be possible to read the BMS here and then update
  * the motor controller. The options are endless. The below code will be updated to show what Collin has done with a reference car
  * in order to show what is possible and how it could be coded. Feel free to delete any code not appropriate to your car and/or
  * add any extra code you need to make GEVCU do what you want.
+ * This is a device like any other so it is disabled by default and will have to be enabled if you need to use it.
 */
 
 #include "VehicleSpecific.h"
@@ -46,6 +47,7 @@ VehicleSpecific::VehicleSpecific() : Device() {
     
     commonName = "VehicleSpecific";
     didInitialSetup = false;
+    waitTicksStartup = (5000000ul / CFG_TICK_INTERVAL_VEHICLE);
 }
 
 /*
@@ -71,50 +73,57 @@ void VehicleSpecific::setup() {
 void VehicleSpecific::handleTick() {
     Device::handleTick(); // Call parent which controls the workflow
     Logger::debug("VS Tick Handler");
+
+    MotorController * motor = deviceManager.getMotorController();
+
+    if (waitTicksStartup > 0) 
+    {
+        waitTicksStartup--;
+        return;
+    }
     
     if (!didInitialSetup)
     {
         didInitialSetup = true;
-        systemIO.setAnalogOut(0, 1); //set first button's LED to RED
+        systemIO.setAnalogOut(1, 1); //set second button's LED to RED
         systemIO.setAnalogOut(0, 1000); //send batch
-    }
-    
-    //button 0 on the pad is neutral
-    //button 1 is drive
-    //button 2 is reverse
-    //button 3 
-    //button 4 
-    //button 5 
-    //button 6 
-    //button 7
-    if (systemIO.getDigitalIn(4)) //set drive mode neutral
-    {
-        Logger::debug("VS Setting gear to neutral");
-        systemIO.setAnalogOut(0, 1);
-        systemIO.setAnalogOut(1, 0);
-        systemIO.setAnalogOut(2, 0);
-        systemIO.setAnalogOut(0, 1000);
-        MotorController * motor = deviceManager.getMotorController();
         if (motor) motor->setSelectedGear(MotorController::NEUTRAL);
     }
-    if (systemIO.getDigitalIn(5)) //set drive mode to drive
+    
+    //button 0 on the pad is reverse
+    //button 1 is neutral
+    //button 2 is drive
+    //button 3 is status
+    //button 4 is cruise set / minus
+    //button 5 is cruise resume / plus
+    //button 6 is turtle mode
+    //button 7 is rabbit mode
+    //There are four digital inputs before us (0-3) so buttons start at digital input 4
+    if (systemIO.getDigitalIn(5)) //set drive mode neutral
     {
-        Logger::debug("VS Setting gear to drive");
+        Logger::debug("VS Setting gear to neutral");
         systemIO.setAnalogOut(0, 0);
         systemIO.setAnalogOut(1, 1);
-        systemIO.setAnalogOut(2, 0);        
+        systemIO.setAnalogOut(2, 0);
         systemIO.setAnalogOut(0, 1000);
-        MotorController * motor = deviceManager.getMotorController();
-        if (motor) motor->setSelectedGear(MotorController::DRIVE);
+        if (motor) motor->setSelectedGear(MotorController::NEUTRAL);
     }
-    if (systemIO.getDigitalIn(6)) //set drive mode to reverse
+    if (systemIO.getDigitalIn(6)) //set drive mode to drive
     {
-        Logger::debug("VS Setting gear to reverse");
+        Logger::debug("VS Setting gear to drive");
         systemIO.setAnalogOut(0, 0);
         systemIO.setAnalogOut(1, 0);
         systemIO.setAnalogOut(2, 1);        
         systemIO.setAnalogOut(0, 1000);
-        MotorController * motor = deviceManager.getMotorController();
+        if (motor) motor->setSelectedGear(MotorController::DRIVE);
+    }
+    if (systemIO.getDigitalIn(4)) //set drive mode to reverse
+    {
+        Logger::debug("VS Setting gear to reverse");
+        systemIO.setAnalogOut(0, 1);
+        systemIO.setAnalogOut(1, 0);
+        systemIO.setAnalogOut(2, 0);        
+        systemIO.setAnalogOut(0, 1000);
         if (motor) motor->setSelectedGear(MotorController::REVERSE);
     }
     
